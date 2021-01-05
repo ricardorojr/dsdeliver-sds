360c21d0281ebb85c6ea342ab2e2c2d0e87f9b96
ricardorojr <ricardorodriguesjr87@gmail.com>
2021-01-05 20:52:07 -0300

    Post/Pedido

--- beckend/src/main/java/com/devsuperior/dsdeliver/controllers/PedidoController.java
+++ beckend/src/main/java/com/devsuperior/dsdeliver/controllers/PedidoController.java
@@ -1,15 +1,21 @@
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.PedidoDTO;
 import com.devsuperior.dsdeliver.services.PedidoService;
+
+
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -22,5 +28,13 @@ public class PedidoController {
 	public ResponseEntity<List<PedidoDTO>> findAll(){
 		List<PedidoDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<PedidoDTO> insert(@RequestBody PedidoDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- beckend/src/main/java/com/devsuperior/dsdeliver/services/PedidoService.java
+++ beckend/src/main/java/com/devsuperior/dsdeliver/services/PedidoService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.PedidoDTO;
+import com.devsuperior.dsdeliver.dto.ProdutoDTO;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
 import com.devsuperior.dsdeliver.entities.Pedido;
+import com.devsuperior.dsdeliver.entities.Produto;
 import com.devsuperior.dsdeliver.repositories.PedidoRepository;
+import com.devsuperior.dsdeliver.repositories.ProdutoRepository;
 
 @Service
 public class PedidoService {
@@ -17,9 +22,24 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repository;
 	
+	@Autowired
+	private ProdutoRepository produtorepository;
+	
 	@Transactional(readOnly = true)
 	public List<PedidoDTO> findAll() {
 		List<Pedido> list = repository.findPedidoswithProdutos();
 		return list.stream().map(x -> new PedidoDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public PedidoDTO insert(PedidoDTO dto) {
+		Pedido pedido = new Pedido(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for(ProdutoDTO p : dto.getProdutos()) {
+			Produto produto = produtorepository.getOne(p.getId());
+			pedido.getProdutos().add(produto);
+		}
+		pedido = repository.save(pedido);
+		return new PedidoDTO(pedido);
 	}
 }
