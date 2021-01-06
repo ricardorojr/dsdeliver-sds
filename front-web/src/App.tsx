e865e626657eb4aef5e48be02bd7c33686df81ad
ricardorojr <ricardorodriguesjr87@gmail.com>
2021-01-06 14:00:41 -0300

    Create react initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './Hello'
+
+function App() {
+  return (
+    <div>
+     <Hello message="Ricardo Rodrigues"/>
+     <Hello message="SDS2"/>
+     <Hello message="React JS"/>
+    </div>
+  );
+}
+
+export default App;
